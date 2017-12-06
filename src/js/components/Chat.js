d6178145f867eb24e05c886c52c0d630a96442a6
pereZZZ <perezzz@meta.ua>
2017-12-06 13:01:20 +0200

    chat begin

--- src/js/components/Chat.js
+++ src/js/components/Chat.js
@@ -1,40 +1,44 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import {Route, Link} from 'react-router-dom'
+import {Route, Link} from 'react-router-dom';
+import io from 'socket.io-client';
+const socket = io('http://localhost:8000');
+// import openSocket from 'socket.io-client';
+// const socket = openSocket('http://localhost:8000');
 
 export default class Chat extends Component {
     constructor(props) {
         super(props);
+        this.state={
+            msgs:[]}
     }
-    shouldComponentUpdate(){return false}
-    lisinig = () => {
-        var socket = io();
-        $('form').submit(function(){
-          socket.emit('chat message', $('#m').val());
-          $('#m').val('');
-          return false;
+
+
+    msgon = () =>{
+        socket.emit('msgtochat',this.refs.msg.value);
+        this.refs.msg.value="";
+    }
+
+    componentDidMount(){
+        var a;
+        socket.on('msgfromchat', (msg) => {
+            a=msg;
+            console.log(a);
+            this.setState({msgs:[...this.state.msgs, a]})
         });
-        socket.on('srat',function(data){
-            console.log(data.lol);
-        })
-        socket.on('chat message', function(data){
-          $('#messages').append('<li><span style="color:red;"> '+data.userid+'</span>'+data.msg+'</li>');
-        });
-        socket.on('bb',function(data){
-          $('#messages').append('<li><span style="color:green;"> '+data+' bb lolka</span></li>');
-        })
     }
 
     render() {
         return (
-            <div>
-                <ul id="messages"></ul>
-                <form action="">
-                    <input id="m" />
-                    <button>Send</button>
-                </form>
-                {this.lisinig}
+            <div className="App">
+                <div id="Allmsg">
+                    {this.state.msgs.map((item)=>{
+                        return <p>{item}</p>
+                    })}
+                </div>
+                <input type="text" className="msg" ref="msg" defaultValue=""/>
+                <button className="msqenter" onClick={this.msgon}>msg</button>
             </div>
         )
     }
